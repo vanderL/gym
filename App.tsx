d922a509db2f848cf55d977fe2884bd17663f64c
Vander Lima <vnd.vander@gmail.com>
2022-11-05 12:47:58 -0300

    build: Native Base

--- App.tsx
+++ App.tsx
@@ -1,26 +1,19 @@
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { Text, View, StatusBar } from "react-native";
-
+import { View, StatusBar } from "react-native";
+import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular, Roboto_700Bold
   })
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Text>Ok, carregou</Text> : <Text>Ok, carregou</Text>}
-    </View>
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
   );
 }
